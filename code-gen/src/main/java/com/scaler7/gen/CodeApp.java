0f48c2c7250f2d238be47986f2abf5f791aa8882
scaler7 <scaler7@163.com>
2020-09-12 14:50:19 +0800

    准备'说说'页面

--- code-gen/src/main/java/com/scaler7/gen/CodeApp.java
+++ code-gen/src/main/java/com/scaler7/gen/CodeApp.java
@@ -2,6 +2,8 @@ package com.scaler7.gen;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
@@ -17,13 +19,13 @@ import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
 
 public class CodeApp {
 	public static void main(String[] args) {
-		// 代码生成器
+		/*// 代码生成器
 		AutoGenerator mpg = new AutoGenerator();
 
 		// 全局配置
 		GlobalConfig gc = new GlobalConfig();
 		String projectPath = System.getProperty("user.dir");
-		gc.setOutputDir(projectPath + "/src/main/java");
+		gc.setOutputDir(projectPath + "/code-gen/src/main/java");
 		gc.setAuthor("scaler7");
 		gc.setOpen(true);
 		gc.setSwagger2(true);// 实体属性 Swagger2 注解
@@ -67,13 +69,13 @@ public class CodeApp {
 						+ tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
 			}
 		});
-		/*
+		*//*
 		 * cfg.setFileCreate(new IFileCreate() {
 		 * 
 		 * @Override public boolean isCreate(ConfigBuilder configBuilder, FileType
 		 * fileType, String filePath) { // 判断自定义文件夹是否需要创建 checkDir("调用默认方法创建的目录");
 		 * return false; } });
-		 */
+		 *//*
 		cfg.setFileOutConfigList(focList);
 		mpg.setCfg(cfg);
 
@@ -88,6 +90,69 @@ public class CodeApp {
 
 		templateConfig.setXml(null);
 		mpg.setTemplate(templateConfig);
+
+		// 策略配置
+		StrategyConfig strategy = new StrategyConfig();
+		strategy.setNaming(NamingStrategy.underline_to_camel);
+		strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+		strategy.setEntityLombokModel(true);
+		strategy.setRestControllerStyle(true);
+		// 写于父类中的公共字段
+		strategy.setControllerMappingHyphenStyle(true);
+		strategy.setTablePrefix(pc.getModuleName() + "_");
+		mpg.setStrategy(strategy);
+		mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+		mpg.execute();*/
+
+		Scanner scanner = new Scanner(System.in);
+
+		// 代码生成器
+		AutoGenerator mpg = new AutoGenerator();
+
+		// 全局配置
+		GlobalConfig gc = new GlobalConfig();
+		String projectPath = System.getProperty("user.dir");
+		gc.setOutputDir(projectPath+"/code-gen/src/main/java"); //生成文件输出目录
+		gc.setAuthor("scaler7");
+		gc.setOpen(false);
+		gc.setSwagger2(true); //实体属性 Swagger2 注解
+		mpg.setGlobalConfig(gc);
+
+		// 数据源配置
+		DataSourceConfig dsc = new DataSourceConfig();
+		dsc.setUrl("jdbc:mysql://127.0.0.1:3306/blog2?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=GMT%2B8");
+		dsc.setDriverName("com.mysql.cj.jdbc.Driver");
+		dsc.setUsername("root");
+		dsc.setPassword("root");
+		mpg.setDataSource(dsc);
+
+		// 包配置
+		PackageConfig pc = new PackageConfig();
+		pc.setParent("com.scaler7");
+		mpg.setPackageInfo(pc);
+		mpg.setPackageInfo(pc);
+		// 自定义配置
+		InjectionConfig cfg = new InjectionConfig() {
+			@Override
+			public void initMap() {
+				// to do nothing
+			}
+		};
+		// 如果模板引擎是 freemarker
+		//String templatePath = "/templates/mapper.xml.ftl";
+		// 自定义输出配置
+		List<FileOutConfig> focList = new ArrayList<>();
+		// 自定义配置会被优先输出
+//        focList.add(new FileOutConfig(templatePath) {
+//            @Override
+//            public String outputFile(TableInfo tableInfo) {
+//                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+//                return projectPath + "/src/main/resources/mapper/" + pc.getModuleName()
+//                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+//            }
+//        });
+		cfg.setFileOutConfigList(focList);
+		mpg.setCfg(cfg); //这个必须要,需要提供一个默认的
 
 		// 策略配置
 		StrategyConfig strategy = new StrategyConfig();
